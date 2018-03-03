50637b422a972080f4a4c2a668983e234dd83223
Ollie Read <ollie@ollieslab.io>
2018-03-03 12:26:54 +0000

    Add a base entity to keep track of dirty state

---
+++ src/Entities/BaseEntity.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ollieread\Articulate\Entities;
+
+abstract class BaseEntity
+{
+    /**
+     * @var array
+     */
+    private $_attributes = [];
+
+    /**
+     * @var array
+     */
+    private $_dirty = [];
+
+    public function set(string $attribute, $value)
+    {
+        $this->_attributes[$attribute] = $value;
+        $this->_dirty[]                = $attribute;
+    }
+
+    public function get(string $attribute)
+    {
+        return $this->_attributes[$attribute] ?? null;
+    }
+
+    public function __get(string $attribute)
+    {
+        return $this->get(snake_case($attribute));
+    }
+
+    public function isDirty(?string $column = null): bool
+    {
+        return $column ? in_array($column, $this->_dirty) : count($this->_dirty) > 0;
+    }
+}
