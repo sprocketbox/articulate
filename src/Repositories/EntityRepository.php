1410384b42086134d8cda3706f78f4e4bbaf8117
Ollie Read <ollie@ollieslab.io>
2018-03-03 12:27:10 +0000

    Add save method for running update/insert using only the entity

--- src/Repositories/EntityRepository.php
+++ src/Repositories/EntityRepository.php
@@ -3,9 +3,11 @@
 namespace Ollieread\Articulate\Repositories;
 
 use Illuminate\Database\DatabaseManager;
+use Ollieread\Articulate\Contracts\Column;
 use Ollieread\Articulate\Database\Builder;
-use Ollieread\Articulate\Entities;
-use Ollieread\Articulate\Mapper;
+use Ollieread\Articulate\Entities\BaseEntity;
+use Ollieread\Articulate\EntityManager;
+use Ollieread\Articulate\Mapping;
 
 class EntityRepository
 {
@@ -20,16 +22,16 @@ class EntityRepository
     private $_database;
 
     /**
-     * @var \Ollieread\Articulate\Entities
+     * @var \Ollieread\Articulate\EntityManager
      */
     private $_manager;
 
     /**
-     * @var \Ollieread\Articulate\Mapper
+     * @var \Ollieread\Articulate\Mapping
      */
     private $_mapper;
 
-    public function __construct(DatabaseManager $database, Entities $manager, Mapper $mapper)
+    public function __construct(DatabaseManager $database, EntityManager $manager, Mapping $mapper)
     {
         $this->_database = $database;
         $this->_manager  = $manager;
@@ -41,12 +43,40 @@ class EntityRepository
     {
         $connection = $this->_mapper->getConnection();
         $table      = $this->_mapper->getTable();
-        $builder    = new Builder($this->_database->connection($connection),
+        $builder    = new Builder(
+            $this->_database->connection($connection),
             $this->_database->getQueryGrammar(),
             $this->_database->getPostProcessor(),
-            $this->_manager,
-            $this->_mapper);
+            $this->_manager);
 
         return $builder->from($table);
+    }
+
+    public function save(BaseEntity $entity)
+    {
+        if (get_class($entity) === $this->_entity) {
+            $keyName = $this->_mapper->getKey();
+            $keyValue = $entity->get($keyName);
+
+            $fields = [];
+
+            $columns = $this->_mapper->getColumns();
+            $columns->each(function (Column $column, string $name) use ($entity, &$fields) {
+                if (! $column->isImmutable() && ! $column->isDynamic() && $entity->isDirty($name)) {
+                    $fields[$name] = $column->toDatabase($entity->get($name));
+                }
+            });
+
+            if (count($fields)) {
+                if (empty($keyValue)) {
+                    $keyValue = $this->query()->insertGetId($fields);
+                    $entity->set($keyName, $keyValue);
+
+                    return $entity;
+                } else {
+                    $this->query()->where($keyName, '=', $keyValue)->update($fields);
+                }
+            }
+        }
     }
 }
