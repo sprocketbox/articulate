ea29d36e8c27c53e70a13b7e1316c84c94ae2252
Ollie Read <ollie@ollieslab.io>
2018-03-12 18:17:15 +0000

    Split out attribute controls for base entity to traits

--- src/Entities/BaseEntity.php
+++ src/Entities/BaseEntity.php
@@ -2,41 +2,9 @@
 
 namespace Ollieread\Articulate\Entities;
 
+use Ollieread\Articulate\Concerns;
+
 abstract class BaseEntity
 {
-    /**
-     * @var array
-     */
-    private $_attributes = [];
-
-    /**
-     * @var array
-     */
-    private $_dirty = [];
-
-    public function set(string $attribute, $value)
-    {
-        $this->_attributes[$attribute] = $value;
-        $this->_dirty[]                = $attribute;
-    }
-
-    public function get(string $attribute)
-    {
-        return $this->_attributes[$attribute] ?? null;
-    }
-
-    public function __get(string $attribute)
-    {
-        return $this->get(snake_case($attribute));
-    }
-
-    public function isDirty(?string $column = null): bool
-    {
-        return $column ? in_array($column, $this->_dirty) : count($this->_dirty) > 0;
-    }
-
-    public function clean()
-    {
-        $this->_dirty = [];
-    }
+    use Concerns\HasAttributes;
 }
