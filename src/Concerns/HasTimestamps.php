59892d40b5a5d74643e1a35dc443417a6a67a2c5
Ollie Read <ollie@ollieslab.io>
2018-03-23 17:28:36 +0000

    Add helper concern for entities with timestamps

---
+++ src/Concerns/HasTimestamps.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ollieread\Articulate\Concerns;
+
+use Carbon\Carbon;
+
+trait HasTimestamps
+{
+    public function getCreatedAt(): Carbon
+    {
+        return $this->get('created_at');
+    }
+
+    public function setCreatedAt(Carbon $value): self
+    {
+        $this->set('created_at', $value);
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): Carbon
+    {
+        return $this->get('created_at');
+    }
+
+    public function setUpdatedAt(Carbon $value): self
+    {
+        $this->set('updated_at', $value);
+
+        return $this;
+    }
+}
