5e785afecfde8c109f4ce47875c55a9a64183460
Ollie Read <ollie@ollieslab.io>
2018-03-23 17:28:50 +0000

    Add contract for repositories that provide auth support

---
+++ src/Contracts/EntityAuthRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Ollieread\Articulate\Contracts;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+
+interface EntityAuthRepository
+{
+    /**
+     * Retrieve a user by their unique identifier.
+     *
+     * @param  mixed  $identifier
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveById($identifier);
+
+    /**
+     * Retrieve a user by their unique identifier and "remember me" token.
+     *
+     * @param  mixed   $identifier
+     * @param  string  $token
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByToken($identifier, $token);
+
+    /**
+     * Update the "remember me" token for the given user in storage.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  string  $token
+     * @return void
+     */
+    public function updateRememberToken(Authenticatable $user, $token);
+
+    /**
+     * Retrieve a user by the given credentials.
+     *
+     * @param  array  $credentials
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByCredentials(array $credentials);
+}
