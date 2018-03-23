1cd43e4dca7981755175a001e291f98cf83334f9
Ollie Read <ollie@ollieslab.io>
2018-03-23 17:25:48 +0000

    Update configuration

--- config/articulate.php
+++ config/articulate.php
@@ -4,6 +4,17 @@ return [
 
     'mappings' => [
 
-    ]
+    ],
+
+    'columns' => [
+        'bool'      => \Ollieread\Articulate\Columns\BoolColumn::class,
+        'entity'    => \Ollieread\Articulate\Columns\EntityColumn::class,
+        'int'       => \Ollieread\Articulate\Columns\IntColumn::class,
+        'json'      => \Ollieread\Articulate\Columns\JsonColumn::class,
+        'string'    => \Ollieread\Articulate\Columns\StringColumn::class,
+        'timestamp' => \Ollieread\Articulate\Columns\TimestampColumn::class,
+    ],
+
+    'auth' => true,
 
 ];
