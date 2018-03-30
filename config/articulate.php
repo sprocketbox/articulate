0e186013fdbbb0a4926e4edeb2881a6f5c058103
Ollie Read <ollie@ollieslab.io>
2018-03-30 23:52:55 +0100

    Add all columns to config

--- config/articulate.php
+++ config/articulate.php
@@ -7,12 +7,16 @@ return [
     ],
 
     'columns' => [
-        'bool'      => \Ollieread\Articulate\Columns\BoolColumn::class,
-        'entity'    => \Ollieread\Articulate\Columns\EntityColumn::class,
-        'int'       => \Ollieread\Articulate\Columns\IntColumn::class,
-        'json'      => \Ollieread\Articulate\Columns\JsonColumn::class,
-        'string'    => \Ollieread\Articulate\Columns\StringColumn::class,
-        'timestamp' => \Ollieread\Articulate\Columns\TimestampColumn::class,
+        'bool'        => \Ollieread\Articulate\Columns\BoolColumn::class,
+        'entity'      => \Ollieread\Articulate\Columns\EntityColumn::class,
+        'int'         => \Ollieread\Articulate\Columns\IntColumn::class,
+        'json'        => \Ollieread\Articulate\Columns\JsonColumn::class,
+        'string'      => \Ollieread\Articulate\Columns\StringColumn::class,
+        'timestamp'   => \Ollieread\Articulate\Columns\TimestampColumn::class,
+        'float'       => \Ollieread\Articulate\Columns\FloatColumn::class,
+        //'object_id'   => \Ollieread\Articulate\Columns\MongoDB\ObjectIdColumn::class,
+        //'subdocument' => \Ollieread\Articulate\Columns\MongoDB\SubdocumentColumn::class,
+        //'utc'         => \Ollieread\Articulate\Columns\MongoDB\UtcColumn::class,
     ],
 
     'auth' => true,
