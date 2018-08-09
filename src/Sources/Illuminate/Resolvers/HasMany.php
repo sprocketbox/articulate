8ad0473fd72cd349d1ae3fac274979696dc6e44b
Ollie Read <ollie@ollieslab.io>
2018-08-09 23:30:26 +0100

    Add has many implementation for illuminate resolvers

--- src/Sources/Illuminate/Resolvers/HasMany.php
+++ src/Sources/Illuminate/Resolvers/HasMany.php
@@ -2,7 +2,35 @@
 
 namespace Sprocketbox\Articulate\Sources\Illuminate\Resolvers;
 
-class HasMany
-{
+use Sprocketbox\Articulate\Contracts\Repository;
 
+class HasMany extends HasOne
+{
+    /**
+     * @param \Sprocketbox\Articulate\Contracts\Repository $repository
+     * @param array                                        $data
+     * @param \Closure|null                                $condition
+     *
+     * @return \Sprocketbox\Articulate\Entities\Entity|\Sprocketbox\Articulate\Support\Collection
+     */
+    public function get(Repository $repository, array $data = [], ?\Closure $condition = null)
+    {
+        $key = $data[$this->localKey] ?? null;
+
+        if ($key) {
+            /**
+             * @var \Sprocketbox\Articulate\Sources\Illuminate\IlluminateBuilder $query
+             */
+            $query = $repository->source()->builder($repository->entity(), $repository->mapping());
+            $query->where($this->foreignKey, '=', $key);
+
+            if ($condition) {
+                $condition($query);
+            }
+
+            return $query->get();
+        }
+
+        return null;
+    }
 }
